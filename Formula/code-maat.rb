26125771d993905c3293318f6072756b94f5e6cb
Sören Blom <soeren.blom@gmail.com>
2017-10-08 00:36:47 +0200

    removes standard comments from Formula

--- Formula/code-maat.rb
+++ Formula/code-maat.rb
@@ -1,7 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class CodeMaat < Formula
   desc "A toolset to mine and analyze version control data"
   homepage "https://github.com/adamtornhill/code-maat"
@@ -19,16 +15,7 @@ class CodeMaat < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test code-maat`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    output = `bin/codemaat -h`
+    output = `#{bin}/code-maat -h`
     assert_match "Version: #{version}", output
   end
 end
